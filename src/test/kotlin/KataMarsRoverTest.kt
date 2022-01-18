bcb4d68752612cb8f4d856ffa2b85cb441aa954a
adrg2 <adr.garro@gmail.com>
2022-01-18 22:14:32 +0100

    Added two commands forward test

--- src/test/kotlin/KataMarsRoverTest.kt
+++ src/test/kotlin/KataMarsRoverTest.kt
@@ -3,6 +3,8 @@ import io.kotest.matchers.shouldBe
 
 class KataMarsRoverTest: FreeSpec({
     "it should verify rover init creation" {
-        kataMarsRover(arrayOf(Command.Forward)) shouldBe Rover(Point(0, 1), Direction.North)
+        kataMarsRover(
+            arrayOf(Command.Forward, Command.Forward)
+        ) shouldBe Rover(Point(0, 2), Direction.North)
     }
 })
