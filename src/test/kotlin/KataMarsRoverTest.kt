eaf4a71bd09a1c96045f7bea30dfb0194192f918
adrg2 <adr.garro@gmail.com>
2022-01-22 12:26:51 +0100

    Refactor dummy objects to move tests
    Signed-off-by: adrg2 <adr.garro@gmail.com>

--- src/test/kotlin/KataMarsRoverTest.kt
+++ src/test/kotlin/KataMarsRoverTest.kt
@@ -2,33 +2,41 @@ import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 
 data class TestData(
-    val rover: Rover = Rover(Point(0,0), Direction.North),
+    val rover: Rover,
     val description: String,
     val commands: List<Command>,
     val expected: Rover
 )
 
 class KataMarsRoverTest : FreeSpec({
+
+    val roverAtZeroZeroNorth = Rover(Point(0,0), Direction.North)
+    val roverAtZeroZeroSouth = Rover(Point(0,0), Direction.South)
+    val roverAtZeroZeroEast = Rover(Point(0,0), Direction.East)
+    val roverAtZeroZeroWest = Rover(Point(0,0), Direction.West)
+
     "Move the skeleton" - {
         listOf(
             TestData(
+                rover = roverAtZeroZeroNorth,
                 description = "increment Y axis when move Forward facing North",
                 commands = listOf(Command.Forward),
                 expected = Rover(Point(0, 1), Direction.North)
             ),
             TestData(
+                rover = roverAtZeroZeroNorth,
                 description = "increment Y axis by 2 when move Forward twice facing North",
                 commands = listOf(Command.Forward, Command.Forward),
                 expected = Rover(Point(0, 2), Direction.North)
             ),
             TestData(
-                rover = Rover(Point(0,0), Direction.South),
+                rover = roverAtZeroZeroSouth,
                 description = "decrement Y axis when move Forward facing South",
                 commands = listOf(Command.Forward),
                 expected = Rover(Point(0, -1), Direction.South)
             ),
             TestData(
-                rover = Rover(Point(0,0), Direction.East),
+                rover = roverAtZeroZeroEast,
                 description = "increment X axis when move Forward facing East",
                 commands = listOf(Command.Forward),
                 expected = Rover(Point(1, 0), Direction.East)
@@ -40,29 +48,31 @@ class KataMarsRoverTest : FreeSpec({
                 expected = Rover(Point(-1, 0), Direction.West)
             ),
             TestData(
+                rover = roverAtZeroZeroNorth,
                 description = "decrement Y axis when move Backward facing North",
                 commands = listOf(Command.Backward),
                 expected = Rover(Point(0, -1), Direction.North)
             ),
             TestData(
+                rover = roverAtZeroZeroNorth,
                 description = "decrement Y axis by 2 when move Backward facing North",
                 commands = listOf(Command.Backward, Command.Backward),
                 expected = Rover(Point(0, -2), Direction.North)
             ),
             TestData(
-                rover = Rover(Point(0,0), Direction.South),
+                rover = roverAtZeroZeroSouth,
                 description = "increment Y axis when move Backward facing South",
                 commands = listOf(Command.Backward),
                 expected = Rover(Point(0, 1), Direction.South)
             ),
             TestData(
-                rover = Rover(Point(0,0), Direction.East),
+                rover = roverAtZeroZeroEast,
                 description = "decrement X axis when move Backward facing East",
                 commands = listOf(Command.Backward),
                 expected = Rover(Point(-1, 0), Direction.East)
             ),
             TestData(
-                rover = Rover(Point(0,0), Direction.West),
+                rover = roverAtZeroZeroWest,
                 description = "increment X axis when move Backward facing West",
                 commands = listOf(Command.Backward),
                 expected = Rover(Point(1, 0), Direction.West)
